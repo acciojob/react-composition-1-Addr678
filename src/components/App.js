f45c3ac4a4d19141ea495b97dbf6d25d458b50e2
Addr678 <im_yadavprashantkumar33@gmail.com>
2023-07-18 09:35:01 +0530

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from 'react';
 
-const App = () => {
+
+const tabs = [
+    { title: 'Tab 1', content: 'This is the content for Tab 1.' },
+    { title: 'Tab 2', content: 'This is the content for Tab 2.' },
+    { title: 'Tab 3', content: 'This is the content for Tab 3.' },
+];
+
+const Tabs = ()=>{
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleClick = (index) => {
+    setActiveTab(index);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <ul>
+        {tabs.map((tab, index) => (
+            //each tab it add into the list with handleClick function
+          <li onClick={() => handleClick(index)}>{tab.title}</li>
+        ))}
+      </ul>
+      <div>{tabs[activeTab].content}</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default Tabs;
